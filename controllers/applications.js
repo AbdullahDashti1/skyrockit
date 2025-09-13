467755998fbb3d79d510fe6a00e2aeef03b6fc3f
Abdulla Dashti <abdullamustafadashti.0@gmail.com>
2025-09-13 13:52:17 +0300

    application submit connection

--- controllers/applications.js
+++ controllers/applications.js
@@ -3,18 +3,34 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 
+// applications create form
+router.get('/new', (req, res) => {
+  res.render('applications/new.ejs');
+});
+
+
 router.get('/', async (req, res) => {
   try {
-    res.render('applications/index.ejs');
+    const currentUser = await User.findById(req.session.user._id);
+    res.render('applications/index.ejs', {
+      applications: currentUser.applications,
+    });
   } catch (error) {
     console.log(error);
     res.redirect('/');
   }
 });
 
-// applications create form
-router.get('/new', (req, res) => {
-  res.render('applications/new.ejs');
+router.post('/', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    currentUser.applications.push(req.body);
+    await currentUser.save();
+    res.redirect(`/users/${currentUser._id}/applications`);
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
 });
 
 module.exports = router;
