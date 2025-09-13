6be62ec7b22189544025b65637700c1dfac1ed21
Abdulla Dashti <abdullamustafadashti.0@gmail.com>
2025-09-13 14:06:54 +0300

    build show page

--- controllers/applications.js
+++ controllers/applications.js
@@ -21,6 +21,18 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/:applicationId', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    const application = currentUser.applications.id(req.params.applicationId);
+    res.render('applications/show.ejs', {
+      application: application,
+    });
+  } catch (error) {
+
+  }
+});
+
 router.post('/', async (req, res) => {
   try {
     const currentUser = await User.findById(req.session.user._id);
